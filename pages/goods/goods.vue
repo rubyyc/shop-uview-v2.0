ae82aadda7b7c74a00cbc1e1eb86de4b22133869
YuanChang <759337828@qq.com>
2021-12-06 16:55:28 +0800

    设置tabbar

---
+++ pages/goods/goods.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		商品
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/profile/profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>个人中心</view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
