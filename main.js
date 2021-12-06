bb049b50b365322c16db4129b46734105d044e53
YuanChang <759337828@qq.com>
2021-12-06 17:22:56 +0800

    1.配置http的拦截响应 2.配置api的集中访问 3.vuex的配置

--- main.js
+++ main.js
@@ -2,11 +2,24 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
+// 假设您项目中已使用VueX
+import store from './store'
+Vue.prototype.$store = store
+
+const vuexStore = require('@/store/$u.mixin.js')
+Vue.mixin(vuexStore)
+
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
+  store,
   ...App
 })
+
+// 引入请求封装，将app参数传递到配置中
+require('@/config/request.js')(app)
 
 app.$mount()
 // #endif
